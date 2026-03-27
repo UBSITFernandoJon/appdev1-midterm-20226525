622f0cfea5e08eec27b144f8e0c2cc046ac9cdf2
UBSITFernandoJon <20226525@s.ubaguio.edu>
2026-03-27 12:38:59 +0800

    Components are routed and connected

--- Task-Manager/src/app/app.routes.ts
+++ Task-Manager/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { TaskAdd } from './components/task-add/task-add';
+import { TaskDetail } from './components/task-detail/task-detail';
+import { TaskEdit } from './components/task-edit/task-edit';
+import { TaskInfo } from './components/task-info/task-info';
+import { TaskList } from './components/task-list/task-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'navbar', component: Navbar},
+    {path: 'task-add', component: TaskAdd},
+    {path: 'task-detail', component: TaskDetail},
+    {path: 'task-edit', component: TaskEdit},
+    {path: 'task-info', component: TaskInfo},
+    {path: 'task-list', component: TaskList}
+];
